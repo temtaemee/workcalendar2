e719a39c8427f06862c3b551a4af20dca8154a08
김영욱 <gim-yeong-ug@gim-yeong-ug-ui-MacBookAir.local>
2025-06-08 20:56:11 +0900

    UPDATE: 캘린더 추가

--- lib/pages/calendar.dart
+++ lib/pages/calendar.dart
@@ -1,16 +1,112 @@
 import 'package:flutter/material.dart';
+import 'package:table_calendar/table_calendar.dart';
+import 'package:intl/intl.dart';
 
-class Calendar extends StatelessWidget {
+class Calendar extends StatefulWidget {
   const Calendar({super.key});
+
+  @override
+  State<Calendar> createState() => _CalendarState();
+}
+
+class _CalendarState extends State<Calendar> {
+  CalendarFormat _calendarFormat = CalendarFormat.month;
+  DateTime _focusedDay = DateTime.now();
+  DateTime? _selectedDay;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Calendar'),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: Text(
+          DateFormat('yyyy년 MM월').format(_focusedDay),
+          style: const TextStyle(
+            color: Colors.black87,
+            fontSize: 20,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
       ),
-      body: const Center(
-        child: Text('Hello from Calendar Page'),
+      body: TableCalendar(
+        firstDay: DateTime.utc(2020, 1, 1),
+        lastDay: DateTime.utc(2030, 12, 31),
+        focusedDay: _focusedDay,
+        calendarFormat: _calendarFormat,
+        selectedDayPredicate: (day) {
+          return isSameDay(_selectedDay, day);
+        },
+        onDaySelected: (selectedDay, focusedDay) {
+          setState(() {
+            _selectedDay = selectedDay;
+            _focusedDay = focusedDay;
+          });
+        },
+        onPageChanged: (focusedDay) {
+          _focusedDay = focusedDay;
+        },
+        calendarBuilders: CalendarBuilders(
+          todayBuilder: (context, day, focusedDay) {
+            return Container(
+              margin: const EdgeInsets.all(4),
+              alignment: Alignment.center,
+              child: Container(
+                width: 25,
+                height: 25,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Colors.blue.withOpacity(0.1),
+                  border: Border.all(
+                    color: Colors.blue.withOpacity(0.5),
+                    width: 0.5,
+                  ),
+                ),
+                child: Text(
+                  '${day.day}',
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ),
+            );
+          },
+          selectedBuilder: (context, day, focusedDay) {
+            return Container(
+              margin: const EdgeInsets.all(4),
+              alignment: Alignment.center,
+              child: Container(
+                // width: 25,
+                // height: 25,
+                alignment: Alignment.center,
+                decoration: BoxDecoration(
+                  // shape: BoxShape.circle,
+                  color: Colors.blue.withOpacity(0),
+                ),
+                child: Text(
+                  '${day.day}',
+                  style: const TextStyle(color: Colors.black),
+                ),
+              ),
+            );
+          },
+        ),
+        calendarStyle: CalendarStyle(
+          outsideDaysVisible: false,
+          weekendTextStyle: const TextStyle(color: Colors.red),
+          holidayTextStyle: const TextStyle(color: Colors.red),
+          cellMargin: EdgeInsets.zero,
+        ),
+        daysOfWeekStyle: const DaysOfWeekStyle(
+          weekdayStyle: TextStyle(color: Colors.black87),
+          weekendStyle: TextStyle(color: Colors.red),
+        ),
+        headerVisible: false,
+        rowHeight: 80,
+        daysOfWeekHeight: 20,
+        pageAnimationEnabled: true,
+        pageAnimationCurve: Curves.easeInOut,
+        pageAnimationDuration: const Duration(milliseconds: 300),
       ),
     );
   }
